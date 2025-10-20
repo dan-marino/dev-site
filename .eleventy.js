2ee18cc36f8d0f99161e06c529ef79c4a6d5a927
Daniel Marino <marinodanielj@hotmail.com>
2025-10-20 01:47:36 -0400

    add CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Copy static assets to _site
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   // Add a global `year` variable
   eleventyConfig.addGlobalData("year", new Date().getFullYear());
